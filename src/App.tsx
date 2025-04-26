23b5aa67642c1389cc5b10b4e8a11e13ffeda98e
Mohammad Qudah <qudahmohammad2002@gmail.com>
2025-04-26 14:49:46 -0400

    deleting bugs

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,16 @@ import { Suspense } from "react";
 import { useRoutes, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import OnboardingFlow from "./components/OnboardingFlow";
-import routes from "tempo-routes";
 import GrantDashboard from "./components/GrantDashboard";
 
 function App() {
   return (
     <Suspense fallback={<p>Loading...</p>}>
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/onboarding" element={<OnboardingFlow />} />
-          <Route path="/dashboard" element={<GrantDashboard />} />
-        </Routes>
-        {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
-      </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/onboarding" element={<OnboardingFlow />} />
+        <Route path="/dashboard" element={<GrantDashboard />} />
+      </Routes>
     </Suspense>
   );
 }
